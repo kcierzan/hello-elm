923d0030c558c2e0ec6f46e29d90ce9c05c4040d
Kyle Cierzan <kcierzan@gmail.com>
2018-04-06 15:52:45 -0400

    initial commit

---
+++ api.js
@@ -0,0 +1,15 @@
+var jsonServer = require('json-server')
+
+
+// Returns and Express server
+var server = jsonServer.create()
+
+
+// Set default middlewares (logger, static, cors, and no-cache)
+server.use(jsonServer.defaults())
+
+var router = jsonServer.router('db.json')
+server.use(router)
+
+console.log('Listening on port 4000')
+server.listen(4000)
---
+++ src/Update.elm
@@ -0,0 +1,19 @@
+module Update exposing (..)
+
+import Models exposing (Model)
+import Msgs exposing (Msg(..))
+import Routing exposing (parseLocation)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Msgs.OnFetchPlayers response ->
+            ( { model | players = response }, Cmd.none )
+
+        Msgs.OnLocationChange location ->
+            let
+                newRoute =
+                    parseLocation location
+            in
+            ( { model | route = newRoute }, Cmd.none )
